52824a2de8a1427047669f0f490d3f08a2390117
Flavio Alles <flavio.alles@proton.me>
2024-03-09 21:14:15 -0300

    Adds new and renames existing examples.

--- examples/custom-resources/delete.sh
+++ examples/custom-resources/delete.sh
@@ -4,5 +4,9 @@ BASE_PATH="examples/custom-resources"
 
 kubectl delete -f $BASE_PATH/email-sender-configs/mail-gun.yaml
 kubectl delete -f $BASE_PATH/email-sender-configs/mailer-send.yaml
-kubectl delete -f $BASE_PATH/emails/mail-gun.yaml
-kubectl delete -f $BASE_PATH/emails/mailer-send.yaml
+kubectl delete -f $BASE_PATH/email-sender-configs/unknown.yaml
+kubectl delete -f $BASE_PATH/emails/a-proper-email-via-mail-gun.yaml
+kubectl delete -f $BASE_PATH/emails/a-failed-email-via-mail-gun.yaml
+kubectl delete -f $BASE_PATH/emails/a-proper-email-via-mailer-send.yaml
+kubectl delete -f $BASE_PATH/emails/a-failed-email-via-mailer-send.yaml
+kubectl delete -f $BASE_PATH/emails/an-email-via-unknown.yaml
